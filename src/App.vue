75319b5e54bc3dd627bf1465ffd9d47ddc175a03
Vadiimmm <vem23@tpu.ru>
2025-07-16 00:00:49 +0500

    to do list

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,74 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <div id="todo-app">
+    <h1>{{ header }}</h1>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+    <div v-if="editing" class="add-task-form">
+      <input v-model="newTask" @keyup.enter="saveTask" placeholder="Add a task" />
+      <label>
+        <input type="checkbox" v-model="newTaskImportant" />
+        Important
+      </label>
+      <button @click="saveTask">Save</button>
     </div>
-  </header>
 
-  <RouterView />
+    <button v-if="editing" @click="doEdit(false)">Cancel</button>
+    <button v-else @click="doEdit(true)">Add Task</button>
+
+    <ul>
+      <li
+        v-for="task in reversedTasks"
+        :key="task.id"
+        :class="{ done: task.completed, important: task.important }"
+        @click="toggleCompleted(task)"
+      >
+        {{ task.label }}
+      </li>
+    </ul>
+  </div>
 </template>
 
+<script setup>
+import { ref, computed } from 'vue'
+
+const header = ref("To-Do List")
+const editing = ref(false)
+const newTask = ref("")
+const newTaskImportant = ref(false)
+
+const tasks = ref([])
+
+const saveTask = () => {
+  if (!newTask.value.trim()) return
+  tasks.value.push({
+    id: tasks.value.length + 1,
+    label: newTask.value,
+    important: newTaskImportant.value,
+    completed: false,
+  })
+  newTask.value = ""
+  newTaskImportant.value = false
+}
+
+const doEdit = (value) => {
+  editing.value = value
+  newTask.value = ""
+  newTaskImportant.value = false
+}
+
+const toggleCompleted = (task) => {
+  task.completed = !task.completed
+}
+
+const reversedTasks = computed(() => [...tasks.value].reverse())
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+.done {
+  text-decoration: line-through;
+  color: gray;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.important {
+  font-weight: bold;
+  color: red;
 }
 </style>
